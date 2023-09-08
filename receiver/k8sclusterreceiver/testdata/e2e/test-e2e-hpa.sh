4ad3d9ef3ca6bb5d9f94f76e7fa6afb0c47f8495
Jina Jain <jjain@splunk.com>
2023-09-08 12:57:44 -0700

    update test script

--- receiver/k8sclusterreceiver/testdata/e2e/test-e2e-hpa.sh
+++ receiver/k8sclusterreceiver/testdata/e2e/test-e2e-hpa.sh
@@ -2,34 +2,74 @@
 
 set -e
 
+GENERATE_EXAMPLE_METRICS=false
 KUBECONFIG=/tmp/kube-config-otelcol-e2e-testing
 
-kind create cluster --config kind-config.yaml --kubeconfig ${KUBECONFIG}
-kind load docker-image otelcontribcol:latest
+setup_cluster() {
+  kind create cluster --config kind-config.yaml --kubeconfig ${KUBECONFIG}
+  kind load docker-image otelcontribcol:latest
+}
 
-# install metrics-server
-wget -O metrics-server/metrics-server.yaml https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-kubectl apply --kustomize metrics-server/
-kubectl wait --for=condition=ready pod -l k8s-app=metrics-server -n kube-system --timeout=120s
+setup_metrics_server() {
+  mkdir tmp-metrics-server
+  wget -O tmp-metrics-server/metrics-server.yaml https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+  cat > tmp-metrics-server/kustomization.yaml <<EOF
+resources:
+  - metrics-server.yaml
+patches:
+  - target:
+      group: apps
+      version: v1
+      kind: Deployment
+      name: metrics-server
+      namespace: kube-system
+    patch: |
+      - op: add
+        path: /spec/template/spec/containers/0/args/-
+        value: --kubelet-insecure-tls
+EOF
+  kubectl apply --kustomize tmp-metrics-server/
+  kubectl wait --for=condition=ready pod -l k8s-app=metrics-server -n kube-system --timeout=120s
+  rm -rf tmp-metrics-server
+}
 
-# setup test hpa and workload
-kubectl apply -f hpa/
-kubectl wait --for=condition=ready pod -l app=busybox --timeout=120s
-kubectl wait --for=condition=ScalingActive hpa -lapp=busybox --timeout=120s
+setup_test_hpa() {
+  kubectl apply -f hpa/
+  kubectl wait --for=condition=ready pod -l app=busybox --timeout=120s
+  kubectl wait --for=condition=ScalingActive hpa -lapp=busybox --timeout=120s
+}
 
-# Run TestE2EHPA
+get_example_metrics() {
+  ## generate expected metrics for hpa
+  DUMPFILE="/tmp/dumpcol/dump.json"
+  shopt -s nullglob
+  for file in ./collector/*; do
+    if [[ $file =~ configmap|deployment ]]; then
+      continue
+    fi
+    sed 's/{{ .Name }}/otel-test/g' $file | kubectl apply -f -
+  done
+  kubectl apply -f hpa_collector.yaml
+  kubectl wait --for=condition=ready pod -l'app.kubernetes.io/name=opentelemetry-collector' --timeout=60s
+
+  while [ ! -s "${DUMPFILE}" ]; do
+      echo "File is empty, waiting for collection"
+      sleep 5
+  done
+  for file in ./collector/*; do
+    sed 's/{{ .Name }}/otel-test/g' $file | kubectl delete -f -
+  done
+  yq -P '.' ${DUMPFILE} -o=yaml > expected_hpa_only.yaml
+}
+
+setup_cluster
+setup_metrics_server
+setup_test_hpa
+if [ "$GENERATE_EXAMPLE_METRICS" == "true" ]; then
+  get_example_metrics
+fi
+
 cd ../..
-go test --run TestE2EHPA e2e_test.go
+go test --run TestE2EHPA e2e_test.go -v
 
-# generate expected metrics for hpa
-#kubectl apply -f  collector-test/
-#kubectl wait --for=condition=ready pod -l'app.kubernetes.io/name=opentelemetry-collector' --timeout=60s
-#DUMPFILE="/tmp/dumpcol/dump.json"
-#while [ ! -s "${DUMPFILE}" ]; do
-#    echo "File is empty, waiting for collection"
-#    sleep 5
-#done
-#kubectl delete -f collector-test/
-#yq -P '.' ${DUMPFILE} -o=yaml > expected_hpa_only.yaml
-
-
+kind delete cluster --name kind
